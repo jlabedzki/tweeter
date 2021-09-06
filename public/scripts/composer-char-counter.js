fbb77087296935594668ff0e432751c4e577c6d4
Johnny <johnnylabedzki@hotmail.com>
2021-09-06 14:48:24 -0700

    Make tweet container. Style articles (each tweet is an article)

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,13 @@
 $(document).ready(() => {
   $('#tweet-text').on('input', function () {
     const input = $(this).val().length;
+
+    //Retreat up the DOM to <form> and traverse down to find the <output>;
     const output = $(this).parent().find('output');
+
     output.html(140 - input);
 
+    //Change text color to red if over text limit
     if (parseInt(output.html()) < 0) {
       $(output).addClass('overCount');
     }
