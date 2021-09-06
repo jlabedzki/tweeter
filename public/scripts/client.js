4309eb83de8768d73dbc08433173bca3f9950a1c
Johnny <johnnylabedzki@hotmail.com>
2021-09-06 11:50:22 -0700

    Change form's input tag to textarea.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,2 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
+$(document).ready(() => {
+});
