62c4ab47ad25f111d24325d8ca2419cdd3609ca7
Johnny <johnnylabedzki@hotmail.com>
2021-09-06 15:42:28 -0700

    Refactor css files as per code review recommendations.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,14 @@
 $(document).ready(() => {
-  $('#tweet-text').on('input', function () {
-    const input = $(this).val().length;
-
-    //Retreat up the DOM to <form> and traverse down to find the <output>;
-    const output = $(this).parent().find('output');
-
-    output.html(140 - input);
-
-    //Change text color to red if over text limit
-    if (parseInt(output.html()) < 0) {
-      $(output).addClass('overCount');
-    }
-  });
+  $('#tweet-text').on('input', charCountAndRecolor);
 });
+
+const charCountAndRecolor = function () {
+  const input = $(this).val().length;
+  //Retreat up the DOM to <form> and traverse down to find the <output>;
+  const output = $(this).parent().find('output');
+  output.html(140 - input);
+  //Change text color to red if over text limit
+  if (parseInt(output.html()) < 0) {
+    $(output).addClass('overCount');
+  }
+};
