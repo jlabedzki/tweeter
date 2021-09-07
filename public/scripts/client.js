a8da18c157db252b9ea89e1ff536d8f87341097e
Johnny <johnnylabedzki@hotmail.com>
2021-09-06 18:33:27 -0700

    Add function to render tweets from an array of tweet objects in client.js. Remove hard coded tweet from index.html.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,10 +5,37 @@ const tweetData = {
     "handle": "@SirIsaac"
   },
   "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+    "text": "If I have seen further it is by standing on the shoulders of giants, yo."
   },
   "created_at": 1461116232227
 }
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
 
 
 const createTweetElement = function (tweetData) {
@@ -22,7 +49,7 @@ const createTweetElement = function (tweetData) {
       </header>
       <p>${tweetData.content.text}</p>
       <footer>
-      <span>10 days ago</span>
+      <span>${timeago.format(tweetData.created_at)}</span>
       <div>
         <i class="fas fa-flag"></i>
         <i class="fas fa-retweet"></i>
@@ -35,30 +62,13 @@ const createTweetElement = function (tweetData) {
   return tweet;
 }
 
-const $tweet = createTweetElement(tweetData);
-
-console.log(tweetData.user.avatars);
+const renderTweets = (tweets) => {
+  for (const tweet of tweets) {
+    const newTweet = createTweetElement(tweet);
+    $('.tweet-container').append(newTweet);
+  }
+};
 
 $(document).ready(() => {
-  $('.tweet-container').append($tweet);
+  renderTweets(data);
 });
-/*
-<article class="tweet">
-<header>
-  <div>
-    <i class="far fa-user"></i>
-    <span>Newton</span>
-  </div>
-  <span class="handle">@SirIsaac</span>
-</header>
-<p>For every action there is an equal an opposite reaction, yo.</p>
-<footer>
-  <span>10 days ago</span>
-  <div>
-    <i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="fas fa-heart"></i>
-  </div>
-</footer>
-</article>
-*/
